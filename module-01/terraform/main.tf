7989d4f147c92758aed321473986a50c86e84201
Denise <deniselewis1120@gmail.com>
2026-01-17 15:31:45 -0800

    terraform

---
+++ module-01/terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "dtc-de-course-484620"
+  region  = "us-central1"
+}
+
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "dtc-de-course-484620-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
