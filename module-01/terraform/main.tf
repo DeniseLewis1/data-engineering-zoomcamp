80063b9423e1cc6678395713561bdc46c00b2ce1
Denise <deniselewis1120@gmail.com>
2026-01-17 16:16:41 -0800

    variables

--- module-01/terraform/main.tf
+++ module-01/terraform/main.tf
@@ -8,14 +8,15 @@ terraform {
 }
 
 provider "google" {
-  project = "dtc-de-course-484620"
-  region  = "us-central1"
+  credentials = file(var.credentials)
+  project = var.project
+  region  = var.region
 }
 
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "dtc-de-course-484620-terra-bucket"
-  location      = "US"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
 
@@ -27,4 +28,11 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
